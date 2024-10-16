dc9c24b0be2ece9527aa2a2f832e61eee1dc8da1
Victor Hugo Tecpa Cisneros <51430198+victor3109@users.noreply.github.com>
2024-10-16 09:51:09 -0600

    Actualizados con output.txt y mediciones.
    Agregue mediciones las cuales se escriben en un .txt, junto con el tiempo de cada parametro. Al ultimo escribiendo tiempo total de ejecucion.

--- SVM_paralelo.py
+++ SVM_paralelo.py
@@ -4,7 +4,7 @@ import time
 from sklearn.svm import SVC
 from sklearn.datasets import make_blobs
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, recall_score, f1_score
 
 # Define the parameter grid for SVM
 param_grid_svm = {
@@ -13,23 +13,36 @@ param_grid_svm = {
     'gamma': [0.001, 0.01, 0.1]  #Se usa para el kernel rbf 
 }
 
+#Funcion para calcular la precision, recall y f1
+def analyze_precision_recall_f1(y_test, y_pred):  
+    recall = recall_score(y_test, y_pred, average='weighted') #Calcula el recall 
+    f1 = f1_score(y_test, y_pred, average='weighted') #Calcula el f1
+    return recall, f1
+
 #Funcion para evaluar SVM para un conjunto de hiperparametros
-def evaluate_svm(hyperparameter_set, X_train, y_train, X_test, y_test, lock):
+def evaluate_svm(hyperparameter_set, X_train, y_train, X_test, y_test, lock,output_file):
     for params in hyperparameter_set:
+        start_time = time.perf_counter()  #tiempo de inicio
+        
         # Se crea el modelo de SVM con los parametros dados 
         # Se excluyen los kernels polinomico y sigmoidal para acelerar el proceso de evaluación 
         if params['kernel'] == 'rbf': #Se evalua si el kernel es rbf para incluir el parametro gamma
             model = SVC(C=params['C'], kernel=params['kernel'], gamma=params['gamma'])
         else: #Si no es rbf, se excluye el parametro gamma
             model = SVC(C=params['C'], kernel=params['kernel'])
-        
+            
         model.fit(X_train, y_train)
         y_pred = model.predict(X_test)
-        accuracy = accuracy_score(y_test, y_pred)
         
+        end_time = time.perf_counter()#Tiempo de fin 
+        
+        accuracy = accuracy_score(y_test, y_pred)
+        recall, f1 = analyze_precision_recall_f1(y_test, y_pred)#Calcular precision, recall y f1
+        time_taken = end_time - start_time#Tiempo total
         #Aqui se imprime el resultado de la evaluación 
         lock.acquire()
-        print(f"Parameters: {params}, Accuracy: {accuracy:.4f}")
+        with open(output_file, "a") as f:
+            f.write(f"Parametros: {params}, Accuracy: {accuracy:.4f}, Recall: {recall:.4f}, F1: {f1:.4f}, Tiempo: {time_taken:.4f} segundos\n")
         lock.release()
 
 # Funcion para nivelar la carga de trabajo entre los procesos, Obtenida de codigo compartido en Classroom
@@ -50,7 +63,7 @@ def nivelacion_cargas(D, n_p):
 
 if __name__ == '__main__':
     #Create a dataset, igual al código compartido en Classroom
-    X, y = make_blobs(n_samples=100000, n_features=50, centers=3, center_box=(-1.0, 1.0))
+    X, y = make_blobs(n_samples=1000, n_features=50, centers=3, center_box=(-1.0, 1.0))
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, stratify=y)
     
     # Generar combinaciones para SVM, Se obtuvo por codigo compartido en Classroom
@@ -58,18 +71,21 @@ if __name__ == '__main__':
     combinations_svm = [dict(zip(keys_svm, v)) for v in itertools.product(*values_svm)]
 
     # Numero de cores (procesos paralelos) a usar
-    N_CORES = 5
+    N_CORES = 10
 
     
     splits = nivelacion_cargas(combinations_svm, N_CORES) #Se divide el grid de parametros entre los procesos
     lock = multiprocess.Lock() #Se crea un lock para evitar problemas de concurrencia en la impresión de resultados
 
+    output_file = "SVM_results.txt"  #Archivo de salida
+    # Limpiar el archivo de salida
+    open(output_file, "w").close()
     
     start_time = time.perf_counter() #Se inicia el tiempo de ejecución
 
     threads = [] #rreglo para guardar los procesos paralelos
     for i in range(N_CORES):
-        threads.append(multiprocess.Process(target=evaluate_svm, args=(splits[i], X_train, y_train, X_test, y_test, lock))) #Se crean los procesos paralelos con la función evaluate_svm
+        threads.append(multiprocess.Process(target=evaluate_svm, args=(splits[i], X_train, y_train, X_test, y_test, lock,output_file))) #Se crean los procesos paralelos con la función evaluate_svm
 
     for thread in threads:
         thread.start()
@@ -78,4 +94,10 @@ if __name__ == '__main__':
         thread.join()
 
     finish_time = time.perf_counter()
+    
+    #Se imprime el tiempo total de ejecución
+    with open(output_file, "a") as f:
+        f.write(f"Tiempo total: {finish_time - start_time:.2f} segundos.\n")
+    
+    output_file.close()
     print(f"Total Time: {finish_time - start_time:.2f} seconds")
--- XGBoost.py
+++ XGBoost.py
@@ -7,37 +7,40 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import precision_score, recall_score, f1_score
 
-# Hyperparameter grid for XGBoost
+#Parametros para XGBoost
 param_grid_xgb = {
-    'n_estimators': [10, 50, 100,150], # Numero de arboles
-    'learning_rate': [0.01, 0.1, 0.2, 0.3], # Tasa de aprendizaje
-    'max_depth': [2,4, 6, 9], # Profundidad maxima de los arboles
-    'subsample': [0.5, 0.7, 0.9, 1.0], # Proporciona de muestras para entrenar
+    'n_estimators': [10, 50, 100, 150], #Numero de arboles a construir
+    'learning_rate': [0.01, 0.1, 0.15, 0.2],#Tasa de aprendizaje para cada arbol
+    'max_depth': [2, 4, 7, 9],#Profundidad maxima de cada arbol
+    'subsample': [0.2, 0.5, 0.8, 1.0]#Fraccion de muestras a usar para entrenar cada arbol
 }
 
-#Calculate precision, recall and f1 score
-def analyze_precision_recall_f1(y_test, y_pred):
-    precision = precision_score(y_test, y_pred, average='weighted')
-    recall = recall_score(y_test, y_pred, average='weighted')
-    f1 = f1_score(y_test, y_pred, average='weighted')
-    return precision, recall, f1
+#FUncion para calcular la precision, recall y f1
+def analyze_precision_recall_f1(y_test, y_pred):  
+    recall = recall_score(y_test, y_pred, average='weighted') #Calcula el recall 
+    f1 = f1_score(y_test, y_pred, average='weighted') #Calcula el f1
+    return recall, f1
 
-#Para ebvaluar el modelo con diferentes hiperparametros
+#funcion para evaluar el modelo XGBoost
 def evaluate_xgb(hyperparameter_set, X_train, y_train, X_test, y_test, lock, output_file):
-    for params in hyperparameter_set:
-        model = xgb.XGBClassifier(**params)
-        model.fit(X_train, y_train)
-        y_pred = model.predict(X_test)
-    
-        precision, recall, f1 = analyze_precision_recall_f1(y_test, y_pred)
-        
-        # Acquiring the lock before writing to the file
+    for params in hyperparameter_set: #iTERAR SOBRE LOS PARAMETROS
+        start_time = time.perf_counter()  #tiempo de inicio
+        model = xgb.XGBClassifier(**params) #Inicializar el modelo
+        model.fit(X_train, y_train) #Entrenar el modelo
+        y_pred = model.predict(X_test)#Predecir con el modelo
+        end_time = time.perf_counter()#Tiempo de fin 
+
+        accuracy = accuracy_score(y_test, y_pred)
+        recall, f1 = analyze_precision_recall_f1(y_test, y_pred)#Calcular precision, recall y f1
+        time_taken = end_time - start_time#Tiempo total
+
+        #Escribe los resultados en el archivo de salida
         lock.acquire()
         with open(output_file, "a") as f:
-            f.write(f"Parametros: {params} Precision: {precision:.4f}, Recall: {recall:.4f}, F1: {f1:.4f}\n")
+            f.write(f"Parametros: {params}, Accuracy: {accuracy:.4f}, Recall: {recall:.4f}, F1: {f1:.4f}, Tiempo: {time_taken:.4f} segundos\n")
         lock.release()
 
-#Funcion para dividir el dataset en partes iguales - Obtenida en Classroom
+#Funcion para nivelar las cargas de trabajo - Obtenido en Classroom
 def nivelacion_cargas(D, n_p):
     s = len(D) % n_p
     n_D = D[:s]
@@ -54,42 +57,48 @@ def nivelacion_cargas(D, n_p):
     return out
 
 if __name__ == '__main__':
-    #Crear dataset
-    X, y = make_blobs(n_samples=10000, n_features=50, centers=3, center_box=(-1.0, 1.0))
+    #Crear un conjunto de datos de prueba
+    X, y = make_blobs(n_samples=1000, n_features=50, centers=3, center_box=(-1.0, 1.0))
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, stratify=y)
     
-    #Generar todas las combinaciones de hiperparametros
+    #Generar todas las combinaciones de parametros - Obtenido del codigo compartido en Classroom
     keys_xgb, values_xgb = zip(*param_grid_xgb.items())
     combinations_xgb = [dict(zip(keys_xgb, v)) for v in itertools.product(*values_xgb)]
 
-    # NUmero de cores
+    #Num de cores
     N_CORES = 10
 
-    #Separa las combinaciones en partes iguales
+    # Nivelar las cargas de trabajo
     splits = nivelacion_cargas(combinations_xgb, N_CORES)
     lock = multiprocess.Lock()
 
-    output_file = "xgboost_results.txt"  #Archivo Output
-
+    output_file = "xgboost_results.txt"  #Archivo de salida
     # Limpiar el archivo de salida
     open(output_file, "w").close()
 
-    # Start timing
+    # Iniciar el temporizador
     start_time = time.perf_counter()
 
-    #Crea los procesos para evaluar el modelo con diferentes hiperparametros
-    threads = [] # Lista de procesos a ejecutar en paralelo
+    # Iniciar los procesos para evaluar el modelo XGBoost
+    threads = []
     for i in range(N_CORES):
-        threads.append(multiprocess.Process(target=evaluate_xgb, args=(splits[i], X_train, y_train, X_test, y_test, lock, output_file))) # Crea un proceso con la funcion evaluate_xgb y los argumentos correspondientes
+        threads.append(multiprocess.Process(target=evaluate_xgb, args=(splits[i], X_train, y_train, X_test, y_test, lock, output_file))) #Crear el proceso con los parametros
 
-    # Para cada proceso en la lista de procesos, se inicia
+    # Iniciar los procesos
     for thread in threads:
         thread.start()
 
-    # Para cada proceso en la lista de procesos, se espera a que termine
+    # Esperar a que todos los procesos terminen
     for thread in threads:
         thread.join()
 
-    # Finish timing
+    # Detener el temporizador
     finish_time = time.perf_counter()
-    print(f"Total Time: {finish_time - start_time:.2f} seconds")
+    
+    #EScribir el tiempo total de ejecución en el archivo de salida
+    with open(output_file, "a") as f:
+        f.write(f"Tiempo total: {finish_time - start_time:.2f} segundos.\n")
+    
+    output_file.close()
+    
+    print(f"Tiempo total: {finish_time - start_time:.2f} segundos.")
